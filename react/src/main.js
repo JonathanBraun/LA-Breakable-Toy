572f454fd231ecfe32b856466b139652702d1e02
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-02-01 11:24:43 -0500

    react troubleshooting update

--- react/src/main.js
+++ react/src/main.js
@@ -1,32 +1,15 @@
-import 'babel-polyfill';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   monsters: []
-    // };
-    this.clickButton = this.clickButton.bind(this)
-  }
-
-
-  componentDidMount() {
-    debugger;
-     this.clickButton();
-   }
-
- clickButton () {
-   let document;
-   document.getElementById('play-button').click();
-   document.getElementById('play-button').click();
-   debugger;
- }
-
-$(function() {
-  ReactDOM.render(
-      <iframe src="https://embed.spotify.com/?uri=spotify:user:jonathanbraun:playlist:02fNngUe73Gf95yUc6Kw00" width="400" height="300" frameborder="0" allowTransparency="true" ></iframe>,
-    document.getElementById('app')
-  );
-});
+// import 'babel-polyfill';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// $(function() {
+//   // if (document.getElementById('app')) {
+//     ReactDOM.render(
+//       // <iframe src="https://embed.spotify.com/?uri=spotify:user:jonathanbraun:playlist:6XIs7oxG4KjWixZsI2F3t8" width="400" height="300" frameBorder="0" allowTransparency="true"></iframe>,
+//       // document.getElementById('app')
+//     );
+//   // }
+// });
+//
+//
+// // flash message at certain times of day
