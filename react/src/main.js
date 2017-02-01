c9a268574db4a692152fe1ffdc1dd3575d9242e7
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-01-31 22:21:11 -0500

    add react to index page

--- react/src/main.js
+++ react/src/main.js
@@ -1,10 +1,32 @@
 import 'babel-polyfill';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   monsters: []
+    // };
+    this.clickButton = this.clickButton.bind(this)
+  }
+
+
+  componentDidMount() {
+    debugger;
+     this.clickButton();
+   }
+
+ clickButton () {
+   let document;
+   document.getElementById('play-button').click();
+   document.getElementById('play-button').click();
+   debugger;
+ }
 
 $(function() {
   ReactDOM.render(
-    <h1>Boo yaa</h1>,
+      <iframe src="https://embed.spotify.com/?uri=spotify:user:jonathanbraun:playlist:02fNngUe73Gf95yUc6Kw00" width="400" height="300" frameborder="0" allowTransparency="true" ></iframe>,
     document.getElementById('app')
   );
 });
