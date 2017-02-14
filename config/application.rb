ce0c8d8d5f52be2dba5d1c23a804c77ea3d01790
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-02-13 19:36:20 -0500

    font fix

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module BreakableToy
     # -- all .rb files in that directory are automatically loaded.
 
     config.assets.enabled = true
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # config.assets.paths << "#{Rails.root}/app/assets/videos"
     config.assets.paths << Rails.root.join('/app/assets/fonts')
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
